0417bdfe162a70b10c5d82f64242ebca30f85749
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-03-29 10:29:56 +0300

    Initial commit

---
+++ HTTPSPostTest/HTTPSPostTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPSPostTest
+//
+//  Created by Stanly Shiyanovskiy on 29.03.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTPSPostTest/HTTPSPostTestTests/HTTPSPostTestTests.m
@@ -0,0 +1,39 @@
+//
+//  HTTPSPostTestTests.m
+//  HTTPSPostTestTests
+//
+//  Created by Stanly Shiyanovskiy on 29.03.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HTTPSPostTestTests : XCTestCase
+
+@end
+
+@implementation HTTPSPostTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
